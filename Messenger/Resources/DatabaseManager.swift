d6beee423c2783702182c51959c288abe4d642e3
bengHak <qkwl4678@naver.com>
2021-08-22 19:51:29 +0900

    feat: ✨ search users

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -16,6 +16,12 @@ final class DatabaseManager {
     // private let database = Database.database().reference()
     // region이 달라서 직접 설정해줘야한다
     private let database = Database.database(url: "https://messenger-96e06-default-rtdb.asia-southeast1.firebasedatabase.app/").reference()
+    
+    static func safeEmail(emailAddress: String) -> String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
 }
 
 
@@ -23,8 +29,7 @@ final class DatabaseManager {
 extension DatabaseManager {
     
     public func userExtists(with email: String, completion: @escaping ((Bool) -> Void)) {
-        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
-        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
         database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? [String: Any] != nil else {
                 completion(false)
@@ -45,8 +50,53 @@ extension DatabaseManager {
                 completion(false)
                 return
             }
+            
+            self.database.child("users").observeSingleEvent(of: .value) { snapshot in
+                
+                let newElement = [
+                    "name": user.firstName + " " + user.lastName,
+                    "email": user.safeEmail
+                ]
+                
+                if var usersCollection = snapshot.value as? [[String:String]] {
+                    // append to user dictionary
+                    usersCollection.append(newElement)
+                    self.database.child("users").setValue(usersCollection, withCompletionBlock: { error, _ in
+                        guard error == nil else {
+                            completion(false)
+                            return
+                        }
+                        completion(true)
+                    })
+                } else {
+                    // create that array
+                    let newCollection: [[String:String]] = [newElement]
+                    self.database.child("users").setValue(newCollection, withCompletionBlock: { error, _ in
+                        guard error == nil else {
+                            completion(false)
+                            return
+                        }
+                        completion(true)
+                    })
+                }
+            }
+            
             completion(true)
         })
+    }
+    
+    public func getAllUsers(completion: @escaping (Result<[[String:String]], Error>) -> Void) {
+        database.child("users").observeSingleEvent(of: .value, with: { snapshot in
+            guard let value = snapshot.value as? [[String:String]] else {
+                completion(.failure(DatabaseError.failedToFetch))
+                return
+            }
+            completion(.success(value))
+        })
+    }
+    
+    public enum DatabaseError: Error {
+        case failedToFetch
     }
 }
 
